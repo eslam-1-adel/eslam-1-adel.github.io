31b4128838e95dd8d2e2e93de4c7b279041aa665
eslam-1-adel <103910161+eslam-1-adel@users.noreply.github.com>
2022-05-09 09:52:27 +0200

    Project

---
+++ project/trending/index.js
@@ -0,0 +1,14 @@
+var i =0;
+var slidesImage = ["images/venom.jpg","images/Spider.jpeg","images/bbbb.jpeg","images/alice slider.jpg","images/hotel.jpg","images/aq.png"]
+var slideShow = function () {
+document.slideshow.src = slidesImage[i];
+if ( i < slidesImage.length-1 )
+{  i++;
+}
+else{
+    i=0;
+}
+
+setTimeout("slideShow()", 2000);
+}
+slideShow();

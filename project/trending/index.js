f09d190a7977e6cb47d8b8093afb7d86089cfe29
eslam-1-adel <103910161+eslam-1-adel@users.noreply.github.com>
2022-05-09 10:10:16 +0200

    Project

--- project/trending/index.js
+++ project/trending/index.js
@@ -1,5 +1,5 @@
 var i =0;
-var slidesImage = ["images/venom.jpg","images/Spider.jpeg","images/bbbb.jpeg","images/alice slider.jpg","images/hotel.jpg","images/aq.png"]
+var slidesImage = ["images/venom.jpg","images/spider.jpeg","images/bbbb.jpeg","images/alice slider.jpg","images/hotel.jpg","images/aq.png"]
 var slideShow = function () {
 document.slideshow.src = slidesImage[i];
 if ( i < slidesImage.length-1 )
